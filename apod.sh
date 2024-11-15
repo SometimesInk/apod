200737c35e72171bdd02bd3c42894e95840b4995
Ink <pandainvasionca@gmail.com>
2024-11-14 19:45:27 -0500

    Logging, successful renaming
    Added logging for:
    - Creation of temp. file tree
    - Downloading images
    - Renaming files
    
    Changed temporary file tree format from:
    .temp/
    to:
    .tp/
    - dl/
    - pr/
    
    Added .gitignore file.

--- apod.sh
+++ apod.sh
@@ -2,24 +2,29 @@
 
 siteUrl="https://apod.nasa.gov/apod/astropix.html"
 allowedTypes="png,jpg,jpeg,webp" # TODO: Config for this
-additionalFlags="-q --show-progress" # TODO: Config for this
+additionalFlags="-q" # TODO: Config for this
 outputName="apod" # TODO: Config for this
 
 # Empty temporary directory if it is already created
-if [[ -e ".temp/" ]]; then
-    echo "Emptying temporary directory..."
-    rm -r .temp/
+if [[ -e ".tp/" ]]; then
+    echo "Emptying temporary directory tree..."
+    rm -r .tp/
 fi
 
 # Create temporary directory
-mkdir .temp/
+echo "Creating temporary directory tree..."
+mkdir -p .tp/dl # Download directory
+mkdir -p .tp/pr # Processing directory
 
 # Download all images of wanted image types while rejecting thumbnail images (which have a lesser quality)
-wget -r -l1 -H -R "*_1024.*" -nd -A $allowedTypes -P ./.temp/ --no-parent --convert-links $additionalFlags $siteUrl
+echo "Downloading images..."
+wget -r -l1 -H -R "*_1024.*" -nd -A $allowedTypes -P ./.tp/dl --no-parent --convert-links $additionalFlags $siteUrl
 
 # Rename image/s to wanted outname followed by the index of the image
+echo "Renaming files..."
 index=0
-for file in ".temp"/*; do
-    mv $file "$outputName_$index"
+for file in ".tp/dl"/*; do
+    ext="${file##*.}"
+    mv $file ".tp/pr/"$outputName"_"$index"."$ext # Rename and move file
     index=$index+1
 done
