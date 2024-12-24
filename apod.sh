7569bf1e38e80d93f6e57eb4c2bf8858152e4050
Ink <pandainvasionca@gmail.com>
2024-12-23 21:14:08 -0500

    Final-ish project
    Added:
    - Error code based logging
    - Better comments and clearer explaining
    - More flags
    - Final command
    
    Changed:
    - Made temporary folder location's modifiable
    
    There is surely more that I missed since I am writing this commit under the intense pressure of the Christmas spirit...

--- apod.sh
+++ apod.sh
@@ -1,23 +1,74 @@
 #!/bin/bash
+#  █████  ██████   ██████  ██████
+# ██   ██ ██   ██ ██    ██ ██   ██
+# ███████ ██████  ██    ██ ██   ██
+# ██   ██ ██      ██    ██ ██   ██
+# ██   ██ ██       ██████  ██████
+#    SCRIPT
+#
+#
+#
+# DESCRIPTION
+# This script downloads NASA's daily image and sets it as your wallpaper
 
-# Flags
+# Initialize flags
+#
+# Cancels messages based on their importance
+# Values: 0 (and less) --- No canceled messages
+#         1            --- Non error messages are canceled
+#         2 (and more) --- No messages are sent
 QUIET=0
+# Creates a copy at the file's location
+# Values: 0 --- No file is created
+#         1 --- File will be copied to script location
 LOCALCOPY=0
+# Changes the final file's location to this one
+OUTPUTFILE=""
 
-# Log function to toggle outputs with -q flag
+# Echoes information messages
+# The '--quiet' flag stops these messages
 function log {
-  if [[ $QUIET -eq 0 ]]; then
+  if [[ $QUIET -lt 1 ]]; then
     echo "$*"
   fi
 }
 
-# Stop function to remove temporary files before exiting
-function stop {
-  if [[ -e .tp/ ]]; then
-    log Removing temporary files...
-    rm -r .tp/
+# Echoes error messages based on an error code
+# The '--quiet-errors' flag stops these messages
+# Messages follow this format: "Error #(Code) --- (Message)"
+function logerr {
+  if [[ $QUIET -lt 2 ]]; then
+    errorMessage="Error #"$2" --- "
+    case $2 in
+    0) # Invalid error number
+      errorMessage=$errorMessage"Invalid error code."
+      ;;
+    1) # Invalid flag/s
+      errorMessage=$errorMessage"Invalid flag/s."
+      ;;
+    2)
+      errorMessage=$errorMessage"Cannot find config file or config file does not exist."
+      ;;
+    3)
+      errorMessage=$errorMessage"Final location does not exist."
+      ;;
+    4)
+      errorMessage=$errorMessage"No valid image found."
+      ;;
+    5)
+      errorMessage=$errorMessage"Cannot successfully correct file."
+      ;;
+    *)
+      logerr 1 0
+      ;;
+    esac
+    echo $errorMessage
   fi
-  exit
+
+  # Stop program
+  if [[ $1 -eq 1 ]]; then
+    exit
+  fi
 }
 
 # Parse flags
@@ -26,38 +77,59 @@ while [[ $# -gt 0 ]]; do
   -q | --quiet)
     QUIET=1
     shift
+    ;;
+  -Q | --quiet-errors)
+    QUIET=2
+    shift
+    ;;
+  -q=* | --quiet=* | -Q=* | --quiet-error=*)
+    QUIET=${1##*=}
     shift
     ;;
   -l | --local-copy)
     LOCALCOPY=1
     shift
+    ;;
+  -o=* | --output-file=*)
+    OUTPUTFILE=${1##*=}
     shift
     ;;
   *)
-    echo "Error: Invalid flag/s."
-    stop
+    logerr 1 1
     ;;
   esac
 done
 
-# Variables
+# Initialize variables
+#
+# The URL used to download the image
 siteUrl="https://apod.nasa.gov/apod/astropix.html"
+# Types to download
 allowedTypes="png,jpg,jpeg"
+# Additional flags to the Wget command
 additionalFlags="-q"
+# Command used to convert the image from its original extension to the wanted one
 conversion="magick" # Either 'convert' [which is deprecated] or 'magick'
+# The name of the output image
 finalName="apod"
+# The extension of the output image
 finalExtension="png"
+# The location of the output image
 finalLocation=""
+# A final command ran at the very end of the file before closing
+finalCommand="hyprpaper"
+# The location used for temporary files (which are automatically deleted)
+temporaryFileLocation="/tmp/apod/"
 
-# Find file location
+# Find this file's location
 thisLocation=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"/"
 
-# Parse config file
+# Parse config file to find variables' values
 if [[ -f $thisLocation"apod.ini" ]]; then
   while read line; do
-    # Check if comment
+    # Ignored commented lines
     if ! [[ ${line%;*} == "" ]]; then
-      # Find KVp
+      # Find key and value of the KVp
       key=${line%=*}
       value=${line##*=}
 
@@ -84,30 +156,49 @@ if [[ -f $thisLocation"apod.ini" ]]; then
       "finalLocation")
         finalLocation=$value
         ;;
+      "finalCommand")
+        finalCommand=$value
+        ;;
+      "temporaryFileLocation")
+        temporaryFileLocation=$value
+        ;;
       esac
     fi
   done <$thisLocation"apod.ini"
 else
-  echo "Error: Cannot find config file or config file does not exist."
-  stop
+  logerr 1 2
+fi
+
+# Stop function to remove temporary files before exiting
+function stop {
+  if [[ -e $temporaryFileLocation ]]; then
+    log Removing temporary files...
+    rm -r $temporaryFileLocation
+  fi
+  exit
+}
+
+# Check for --output-file flag
+if ! [[ $OUTPUT == "" ]]; then
+  log Changing final location to output...
+  finalLocation=$OUTPUT
 fi
 
 # Check if final location exists
 if ! [[ -d $finalLocation ]]; then
-  echo "Error: Final location $finalLocation does not exist."
-  stop
+  logerr 1 3
 fi
 
 # Empty temporary directory if it is already created
-if [[ -e ".tp/" ]]; then
-  log Emptying temporary directory tree.
-  stop
+if [[ -e $temporaryFileLocation ]]; then
+  log Emptying temporary directory tree...
+  rm -r $temporaryFileLocation
 fi
 
 # Create temporary directory
 log Creating temporary directory tree...
-mkdir -p .tp/dl # Download directory
-mkdir .tp/pr    # Processing directory
+mkdir -p $temporaryFileLocation"dl" # Download directory
+mkdir -p $temporaryFileLocation"pr" # Processing directory
 
 # Download all images of wanted image types while rejecting thumbnail images (which have a lesser quality)
 log Downloading files...
@@ -120,36 +211,39 @@ log Downloading files...
 # -R ; don't download any file matching the string "*_1024.*" (thumbnail images)
 # -A ; only download files of accepted file extensions
 # --convert-links ; fix web links to make for usable images
-wget -p --no-parent -nd -P ./.tp/dl -e robots=off -R "*_1024.*" -A $allowedTypes --convert-links $additionalFlags $siteUrl
+wget -p --no-parent -nd -P $temporaryFileLocation"dl" -e robots=off -R "*_1024.*" -A $allowedTypes --convert-links $additionalFlags $siteUrl
 
 # Rename image/s to wanted outname followed by the index of the image
 # This it done to make it easier to handle files without having to deal with different file extensions
 log Renaming files...
 index=0
-for file in ".tp/dl"/*; do
+for file in $temporaryFileLocation"dl"/*; do
   ext="${file##*.}"
-  fileName=".tp/pr/"$finalName"_"$index"."
+  fileName=$temporaryFileLocation"pr/"$finalName"_"$index"."
   mv $file $fileName$ext                                  # Rename and move file
   eval $conversion $fileName$ext $fileName$finalExtension # Change file extension
   rm $fileName$ext
   index=$index+1
 done
 
-# Local-copy flag
+# Check for --local-copy flag
 if [[ LOCALCOPY -eq 1 ]]; then
-  log Copying "local" copy...
+  log Copying local copy...
   cp $fileName$finalExtension $finalName"."$finalExtension
 fi
 
-# Select file
+# Move file to its final location
 if [[ $index -eq 0 ]]; then
-  echo "Error: No valid image found."
+  logerr 1 4
 elif [[ $index -ne 1 ]]; then
-  echo "Error: Selecting file..."
+  logerr 1 5
 else
   log Moving and converting file...
   mv $fileName$finalExtension "$finalLocation/$finalName.$finalExtension"
 fi
 
-# End it all
+# Final sentence
+eval $finalCommand
+
+# The end it all
 stop
